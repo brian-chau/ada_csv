5779aeb728e7cf5264ce8dd5234f9eeac2aa5c9d
Brian Chau <brian.chau@outlook.com>
2021-08-08 15:37:28 -0700

    First commit

---
+++ obj/clean.bat
@@ -0,0 +1,12 @@
+@echo off
+
+del *.o
+del *.adb.stdout
+del *.adb.stderr
+del default-loc.xml
+del *.ali
+del *.bexch
+del *.ads
+del *.adb
+
+pause
---
+++ obj/run.bat
@@ -0,0 +1,5 @@
+REM @echo off
+
+main.exe < test.csv
+
+pause
